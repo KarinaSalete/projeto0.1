0fc1486f998a0a4089a88fba8b170b739d6a1ee6
Karina Salete <karinasalete@gmail.com>
2022-05-25 12:33:37 -0300

    commit inicial

---
+++ aulas10-20/aula12.1/js.js
@@ -0,0 +1,11 @@
+ let num1 = prompt('numero ?')
+ let num2 = prompt('numero ?')
+ num1 = Number(num1)
+ num2 = Number(num2)
+
+
+ console.log(num1 + num2)
+ const resultado = num1 + num2
+
+
+ alert(`o resultado de sua conta é ${resultado}`)
---
+++ aulas10-20/aula12/aula.js
@@ -0,0 +1,10 @@
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+const varD = varC
+const varE = varA
+const varF = varB
+
+
+console.log(varF, varD, varE)
